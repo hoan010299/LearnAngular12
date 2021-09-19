9c26a2c1103a08c32974376fd42feb4b15670f1e
hoan <hoan010299@gmail.com>
2021-09-19 19:03:57 +0700

    ng-template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
@@ -14,7 +15,8 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,14 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
- public name = 'Hoan Nguyen';
- public age = 20;
-  constructor() { }
+  public name = 'Hoan Nguyen';
+  public age = 20;
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public resetAge() {
+    this.age = 0;
   }
-
 }
